48a90c53d6b836a50ceb25e4d960054548c794e3
Matheus Gonçalves <goncalvesouza1996@gmail.com>
2024-07-29 22:37:43 -0300

    feat: modulação do css

--- src/js/auth.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('login-form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    // Aqui você pode adicionar a lógica de autenticação
-    alert(`Login realizado com sucesso para o usuário ${username}`);
-});
